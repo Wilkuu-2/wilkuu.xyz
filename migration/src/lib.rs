8814def37ee1522cd39f7763f230571ffcb70418
Jakub Stachurski <jakub@wilkuu.xyz>
2024-01-31 20:04:45 +0100

    Create the user model

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,12 +1,16 @@
 pub use sea_orm_migration::prelude::*;
 
 mod m20240119_130354_create_posts;
+mod m20240131_114949_create_user_table;
 
 pub struct Migrator;
 
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![Box::new(m20240119_130354_create_posts::Migration)]
+        vec![
+            Box::new(m20240119_130354_create_posts::Migration),
+            Box::new(m20240131_114949_create_user_table::Migration),
+        ]
     }
 }
